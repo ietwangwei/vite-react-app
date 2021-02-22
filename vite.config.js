38848e045c10c1d17ad21bfc8c295dfe125c2de3
wei_gao <ACI_weigao@gaoweideMacBook-Pro.local>
2021-02-22 13:29:46 +0800

    feat: add http2 support

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import reactRefresh from '@vitejs/plugin-react-refresh';
+import fs from 'fs';
 import path from 'path';
 
 import env from './env';
@@ -12,6 +13,17 @@ const resolve = dir => path.join(__dirname, dir);
  * @type { import('vite').UserConfig }
  */
 export default {
+  server: {
+    port: 8005,
+    https: {
+      ServerOptions: {
+        ca: fs.readFileSync(resolve('config/rootCA.pem')),
+        key: fs.readFileSync(resolve('config/privatekey.pem')),
+        cert: fs.readFileSync(resolve('config/certificate.pem')),
+      },
+    },
+  },
+
   plugins: [reactRefresh()],
   base: TARGET === 'github' ? '/vite-react-app' : '/',
   resolve: {
