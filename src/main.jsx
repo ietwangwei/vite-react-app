6bd63b87049d5e5624dc4af76523aef4c0578283
gaowei <wei.gao@gaoweideMacBook-Pro.local>
2021-03-01 14:46:48 +0800

    refactor: refactor react root container

--- src/main.jsx
+++ src/main.jsx
@@ -8,11 +8,15 @@ import App from './App.jsx';
 import 'styles/main.scss';
 import './assets/fonts/if.scss';
 
+const rootEl = document.createElement('div');
+rootEl.id = 'root';
+document.body.insertBefore(rootEl, document.body.childNodes[0]);
+
 ReactDOM.render(
   <StrictMode>
     <App />
   </StrictMode>,
-  document.getElementById('root')
+  rootEl
 );
 
 // eslint-disable-next-line no-console
