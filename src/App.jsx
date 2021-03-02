a96a54ef782e83324fa508dcf9f3c4f115f44bef
gaowei <wei.gao@gaoweideMacBook-Pro.local>
2021-03-02 15:54:48 +0800

    feat: add network request progress bar

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,19 @@ import { renderRoutes } from 'react-router-config';
 import { BrowserRouter } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 import routes from 'routes/index';
+import useProgress from 'use-progress';
 
+import 'use-progress/dist/use-progress.css';
 import 'styles/App.scss';
 
-const App = () => (
-  <RecoilRoot>
-    <BrowserRouter>{renderRoutes(routes)}</BrowserRouter>
-  </RecoilRoot>
-);
+const App = () => {
+  useProgress();
+
+  return (
+    <RecoilRoot>
+      <BrowserRouter>{renderRoutes(routes)}</BrowserRouter>
+    </RecoilRoot>
+  );
+};
 
 export default App;
