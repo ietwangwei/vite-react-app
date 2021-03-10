3409260a327000ea00a3427a16bfc2e51a4c3e66
qinghuanI <45612221+qinghuanI@users.noreply.github.com>
2021-03-10 07:51:57 +0000

    Deploying to gh-pages from @ qinghuanI/vite-react-app@3d5e516195eff9d83210b93bf1cdbf7c7268122d 🚀

---
+++ assets/index.1e9b41eb.js
@@ -0,0 +1,1 @@
+import"./index.e83b9331.js";import"./vendor.a989d542.js";export default()=>React.createElement("div",null,"this is Page2 component");
--- assets/index.d46d443a.js
+++
@@ -1,1 +0,0 @@
-import"./index.df301fda.js";import"./vendor.a989d542.js";export default()=>React.createElement("div",null,"this is Page2 component");
